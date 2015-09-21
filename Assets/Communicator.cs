4981f23241928e217576c8919eb3047f10a0dd52
siloenaps <str@siloen.dk>
2015-09-21 17:08:06 +0200

    Marc did som changes to Stream.cs
    Those changes are now reflected in the Unity implementation.

--- Assets/Communicator.cs
+++ Assets/Communicator.cs
@@ -6,18 +6,22 @@ using System.Globalization;
 
 public class Communicator : MonoBehaviour{
 	private Stream stream;
-	private IEnumerator coroutine;
+	private IEnumerator coroutinePush;
+	private IEnumerator coroutinePull;
+
+	public delegate void MyDelegate(string msg);
+	MyDelegate myDelegate;
 
 	void Start () {
 		// Create socket stream object
-		stream = new Stream();
+		stream = new Stream("one");
 
-		// (Coroutine is for testing purposes. Sends message once per second)
-		coroutine = Send (1.0F);
-		StartCoroutine(coroutine);
+		// PUSH message once per second test
+		coroutinePush = Push (1.0F);
+		StartCoroutine(coroutinePush);
 	}
 
-	IEnumerator Send(float delay) {
+	IEnumerator Push(float delay) {
 		while (true) {
 			yield return new WaitForSeconds (delay);
 
@@ -25,37 +29,33 @@ public class Communicator : MonoBehaviour{
 			float z = Random.Range(-3.0F, 3.0F);
 
 			// Send the message
-			stream.send ("name=two&message="+x+","+z);
+			stream.Send ("name=two&message="+x+","+z);
 		}
 	}
 
 	void Update () {
 		// Using Update for pulling feed from Stream 
-		string feed = stream.buffer.Get ();
-		if (feed != null && feed != "noop") {
-
-			char[] del = { ',' };
-			string[] arr = feed.Split(del);
-			Vector3 pos = gameObject.transform.position;
-			pos.x = float.Parse(arr[0], CultureInfo.InvariantCulture.NumberFormat);;
-			pos.z = float.Parse(arr[1], CultureInfo.InvariantCulture.NumberFormat);;
-			gameObject.transform.position = pos;
+		string[] received = stream.Receive();
+		
+		if(received != null) {
+			for(int i = 0; i < received.Length; i++) {
+				string feed = received[i];
+				Debug.Log ("Update: "+feed);
+				if (feed != null && feed != "noop") {					
+					char[] del = { ',' };
+					string[] arr = feed.Split(del);
+					Vector3 pos = gameObject.transform.position;
+					pos.x = float.Parse(arr[0], CultureInfo.InvariantCulture.NumberFormat);;
+					pos.z = float.Parse(arr[1], CultureInfo.InvariantCulture.NumberFormat);;
+					gameObject.transform.position = pos;
+				}
+			}
 		}
 
 		if (Input.GetKeyDown("space")){
 			// (Stopping coroutine stops loop sending messages)
-			StopCoroutine(coroutine);
-			stream.Close();
+			StopCoroutine(coroutinePush);
 			Debug.Log ("Stop");
 		}
 	}
-
-	#region Async implementation
-
-	public void Receive (string message)
-	{
-		Debug.Log ("Receive: "+message);
-	}
-
-	#endregion
 }
--- Assets/Stream.cs
+++ Assets/Stream.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Net;
 using System.Net.Sockets;
+using System.Collections.Generic;
 using System.Threading;
 using System.Text;
 
@@ -9,131 +10,108 @@ using System.Text;
  * For unity policy check, uncomment lines with: // policy
  * For usage scroll down to main() method.
  */
-using UnityEngine;
-using System.Collections.Generic;
-
 public class Stream {
-    public String host = "talk.rupy.se";
-    public int port = 80;
-
-    private Socket pull, push;
-
-    public class State {
-        public Socket socket = null;
-        public const int size = 32768;
-        public byte[] data = new byte[size];
-    }
-
-	public Buffer buffer;
-	public class Buffer {
-		public List<string> queue;
-		public Buffer(){
-			queue = new List<string>();
-		}
-		public void Add(string msg){
-			try{
-				queue.Add (msg);
-			} catch (Exception e) {  
-				Debug.LogException(e);
-			}
-		}
-		public string Get(){
-			string msg = null;
-			if (queue.Count > 0) {
-				try{
-					msg = queue[0];
-					queue.RemoveAt(0);
-				} catch (Exception e) {  
-					Debug.LogException(e);
-				}
-			}
-			return msg;
-		}
+	public string host = "talk.rupy.se";
+	public int port = 80;
+	
+	private Queue<string> queue;
+	private Socket pull, push;
+	
+	private class State {
+		public Socket socket = null;
+		public const int size = 32768;
+		public byte[] data = new byte[size];
 	}
 	
-	public Stream() {
-        bool policy = true;
-
-		buffer = new Buffer();
-
-
-        //policy = Security.PrefetchSocketPolicy(host, port); // policy
-        if(!policy)
-            throw new Exception("Policy (" + host + ":" + port + ") Failed");
-
-        IPAddress address = Dns.Resolve(host).AddressList[0];
-        IPEndPoint remote = new IPEndPoint(address, port);
-
-        push = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
-        pull = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
-
-        push.Connect(remote);
-        pull.Connect(remote);
-
-        String data = "GET /pull?name=one HTTP/1.1\r\n"
-                    + "Host: " + host + "\r\n"
-                    + "Head: less\r\n\r\n"; // enables TCP no delay
-
-        int sent = pull.Send(Encoding.ASCII.GetBytes(data));
-
-        State state = new State();
-        state.socket = pull;
-
-        pull.BeginReceive(state.data, 0, State.size, 0, new AsyncCallback(callback), state);
-    }
-
-    /*
-     * Not thread safe, only call from one thread.
-     */
-    public void send(String message) {
-        byte[] data = new byte[64];
-        String text = "POST /push HTTP/1.1\r\n"
-                    + "Host: " + host + "\r\n"
-                    + "Head: less\r\n\r\n" // enables TCP no delay
-                    + message;
-
-        int sent = push.Send(Encoding.ASCII.GetBytes(text));
-        int read = push.Receive(data);
-    }
-
-    private void callback(IAsyncResult ar) {
-		try {
-            State state = (State) ar.AsyncState;
-            int read = state.socket.EndReceive(ar);
-
-            if(read > 0) {
-                String text = Encoding.ASCII.GetString(state.data, 0, read);
-                String[] split = text.Split(new string[] { "\r\n" }, StringSplitOptions.None);
-
-                if(!split[0].StartsWith("HTTP")) {
-                    int length = split[0].Length + int.Parse(split[0]) + 4;
-
-                    if(length != read) {
-						Debug.Log("AooB: " + read + "/" + length);
-                    }
-
-                    String[] messages = split[1].Split('\n');
-
-                    for(int i = 0; i < messages.Length; i++) {
-                        if(messages[i].Length > 0) {
-//                            this.async.Receive(messages[i]);
-							buffer.Add(messages[i]);
-                        }
-                    }
-                }
-
-                state.socket.BeginReceive(state.data, 0, State.size, 0, new AsyncCallback(callback), state);
-            }
-        } catch (Exception e) {            
-			Debug.LogException(e);
-        }
-    }
-
-	public void Close(){
-		// Release the socket.
-		push.Shutdown(SocketShutdown.Both);
-		push.Close();
-		pull.Shutdown(SocketShutdown.Both);
-		pull.Close();
+	public Stream(String name) {
+		queue = new Queue<string>();
+		
+		bool policy = true;
+		
+		//policy = Security.PrefetchSocketPolicy(host, port); // policy
+		
+		if(!policy)
+			throw new Exception("Policy (" + host + ":" + port + ") failed.");
+		
+		IPAddress address = Dns.Resolve(host).AddressList[0];
+		IPEndPoint remote = new IPEndPoint(address, port);
+		
+		push = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+		pull = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+		
+		push.Connect(remote);
+		pull.Connect(remote);
+		
+		String data = "GET /pull?name=" + name + " HTTP/1.1\r\n"
+			+ "Host: " + host + "\r\n"
+				+ "Head: less\r\n\r\n"; // enables TCP no delay
+		
+		pull.Send(Encoding.ASCII.GetBytes(data));
+		
+		State state = new State();
+		state.socket = pull;
+		
+		pull.BeginReceive(state.data, 0, State.size, 0, new AsyncCallback(Callback), state);
 	}
-}
+	
+	public void Send(String message) {
+		byte[] data = new byte[64];
+		String text = "POST /push HTTP/1.1\r\n"
+			+ "Host: " + host + "\r\n"
+				+ "Head: less\r\n\r\n" // enables TCP no delay
+				+ message;
+		
+		push.Send(Encoding.ASCII.GetBytes(text));
+		push.Receive(data);
+	}
+	
+	public string[] Receive() {
+		lock(queue) {
+			if(queue.Count > 0) {
+				string[] messages = new string[queue.Count];
+				
+				for(int i = 0; i < messages.Length; i++) {
+					messages[i] = queue.Dequeue();
+				}
+				
+				return messages;
+			}
+		}
+		
+		return null;
+	}
+	
+	private void Callback(IAsyncResult ar) {
+		try {
+			State state = (State) ar.AsyncState;
+			int read = state.socket.EndReceive(ar);
+			
+			if(read > 0) {
+				String text = Encoding.ASCII.GetString(state.data, 0, read);
+				String[] split = text.Split(new string[] { "\r\n" }, StringSplitOptions.None);
+				
+				if(!split[0].StartsWith("HTTP")) {
+					int length = split[0].Length + int.Parse(split[0]) + 4;
+					
+					if(length != read) {
+						System.Console.WriteLine("AooB: " + read + "/" + length);
+					}
+					
+					String[] messages = split[1].Split('\n');
+					
+					lock(queue) {
+						for(int i = 0; i < messages.Length; i++) {
+							if(messages[i].Length > 0) {
+								queue.Enqueue(messages[i]);
+							}
+						}
+					}
+				}
+				
+				state.socket.BeginReceive(state.data, 0, State.size, 0, new AsyncCallback(Callback), state);
+			}
+		} catch (Exception e) {
+			Console.WriteLine(e.ToString());
+		}
+	}
+};
